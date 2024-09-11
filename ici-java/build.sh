13da8ae068101b6352528571b645e73746aeab08
hanl5 <hanl5@xiaopeng.com>
2024-09-11 15:17:12 +0800

    u

--- ici-java/build.sh
+++ ici-java/build.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+set -e
+SCRIPT_PATH="$(
+    cd "$(dirname "$0")" >/dev/null 2>&1 || exit
+    pwd -P
+)"
+cd "$SCRIPT_PATH" || exit
+java -version
 mvn clean package appassembler:assemble
